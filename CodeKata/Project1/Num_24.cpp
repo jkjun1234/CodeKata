5e05fa34ac68e4d601a712ee7fa260c85faa1e20
JunCo <jkjun1234@gmail.com>
2025-06-27 10:19:29 +0900

    Num24
    aa

---
+++ CodeKata/Project1/Num_24.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+    string answer = "";
+
+    for (int x = 0; x < seoul.size(); x++)
+    {
+        if ("Kim" == seoul[x])
+        {
+            answer.append("""김서방은 ").append(to_string(x)).append("에 있다""");
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    vector<string> v = { "Jun", "Kim" };
+
+    cout << solution(v);
+
+    return 0;
+}

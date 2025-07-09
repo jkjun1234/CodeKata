1e66c20d6ae8a423d13e88f33f0c9ac2712d6322
JunCo <jkjun1234@gmail.com>
2025-07-09 10:07:08 +0900

    num 33 commit

---
+++ CodeKata/Project1/Num_33.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+int factors(int left, int right) {
+    int count = 0, result = 0;
+
+    for (left; left <= right; left++) {    //left ~ right ¼øÈ¯
+        for (int i = 1; i <= left; i++) {  // 1~left ¼øÈ¯
+            if (left % i == 0) { // ¾à¼ö
+                count++;   //¾à¼ö °¹¼ö Ä«¿îÆ®
+            }
+        }
+
+        if (count % 2 == 0) {//Â¦¼ö
+            result += left; //Â¦¼öÀÏ°æ¿ì ¿ÞÂÊ ¼ö ´õÇÔ
+        }
+        else {
+            result -= left; //È¦¼ö »©±â
+        }
+        count = 0;  // count ÃÊ±âÈ­
+    }
+
+    return result;
+}
+//
+int solution(int left, int right) {
+    int answer = 0;
+
+    return answer = factors(left, right);
+}
+
+
+int main() {
+    cout << "13~17 ¾à¼öÀÇ È¦¼ö Â¦¼ö µ¡¼À : " << solution(13, 17) << endl;
+
+	return 0;
+}
